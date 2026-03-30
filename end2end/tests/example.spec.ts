c38f1a33ccfc998a16d49d6c3e372fcfea174c2e
Michael Alan Dorman <mdorman@ironicdesign.com>
2026-03-30 08:25:18 -0400

    Reformat end2end test files with prettier

--- end2end/tests/example.spec.ts
+++ end2end/tests/example.spec.ts
@@ -7,9 +7,9 @@ test("homepage has title and links to intro page", async ({ page }) => {
 
   await expect(page.locator("h1")).toHaveText("Welcome to Leptos!");
 
-  await expect(page.getByRole('button')).toHaveText("Click Me: 0");
+  await expect(page.getByRole("button")).toHaveText("Click Me: 0");
 
-  await page.click('button');
+  await page.click("button");
 
-  await expect(page.getByRole('button')).toHaveText("Click Me: 1");
+  await expect(page.getByRole("button")).toHaveText("Click Me: 1");
 });
